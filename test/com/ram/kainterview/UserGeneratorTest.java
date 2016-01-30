168fd6209c5391123f5ab7fc25763ba6d20824fc
ramv13 <ramsai.vellanki@gmail.com>
2016-01-29 23:52:36 -0500

    Added package level javadocs

--- test/com/ram/kainterview/UserGeneratorTest.java
+++ test/com/ram/kainterview/UserGeneratorTest.java
@@ -1,7 +1,24 @@
+/**
+ * Package for the infection implementations for the Khan Academy interview
+ * 
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2016 Ram Vellanki
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ */
 package com.ram.kainterview;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -88,6 +105,38 @@ public class UserGeneratorTest {
 	@Test(expected=IllegalArgumentException.class)
 	public void testInvalidLevels() {
 		UserGenerator.generateUsers(1, 1, 0);
+	}
+	
+	/**
+	 * Generates a specific user graph
+	 * 
+	 *   	coach1				  	  coach2
+	 * student1 student2  		student3 student4	 
+	 * 
+	 * @return the list of all users in the graph
+	 */
+	public static List<User> generateSpecificUsers() {
+		List<User> users = new ArrayList<>(6);
+		
+		User coach1 = new User();
+		User student1 = new User();
+		User student2 = new User();
+		coach1.addStudent(student1);
+		coach1.addStudent(student2);
+		
+		User coach2 = new User();
+		User student3 = new User();
+		User student4 = new User();
+		coach2.addStudent(student3);
+		coach2.addStudent(student4);
+		
+		users.add(coach1);
+		users.add(student1);
+		users.add(student2);
+		users.add(coach2);
+		users.add(student3);
+		users.add(student4);
+		return users;
 	}
 	
 }
